e82cc236b67f1ea071e818a44112411199236d58
moon <mym1207@plyxity.com>
2025-12-23 16:04:10 +0900

    refactor: CctvServiceKoTest 테스트케이스 보강

--- src/test/kotlin/com/pluxity/aiot/cctv/entity/DummyEntities.kt
+++ src/test/kotlin/com/pluxity/aiot/cctv/entity/DummyEntities.kt
@@ -2,6 +2,7 @@ package com.pluxity.aiot.cctv.entity
 
 import com.pluxity.aiot.base.entity.withId
 import com.pluxity.aiot.cctv.Cctv
+import com.pluxity.aiot.site.Site
 
 fun dummyCctv(
     id: Long = 1L,
@@ -9,10 +10,14 @@ fun dummyCctv(
     url: String = "url",
     lon: Double = 127.0,
     lat: Double = 37.0,
+    mtxName: String? = null,
+    site: Site? = null,
 ): Cctv =
     Cctv(
         name = name,
         url = url,
         longitude = lon,
         latitude = lat,
+        mtxName = mtxName,
+        site = site,
     ).withId(id)

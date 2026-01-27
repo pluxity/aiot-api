cac51f48a8efee77dae77bd48c82154d5841374b
moon <>
2026-01-27 14:17:45 +0900

    refactor: @field:NotNull 어노테이션 제거 및 var → val 변경

--- src/main/kotlin/com/pluxity/aiot/permission/dto/PermissionGroupCreateRequest.kt
+++ src/main/kotlin/com/pluxity/aiot/permission/dto/PermissionGroupCreateRequest.kt
@@ -1,15 +1,12 @@
 package com.pluxity.aiot.permission.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotNull
 
 data class PermissionGroupCreateRequest(
     @field:Schema(description = "권한 집합 이름")
-    @field:NotNull
-    var name: String,
+    val name: String,
     @field:Schema(description = "권한에 대한 설명")
     val description: String?,
     @field:Schema(description = "권한의 상세 목록")
-    @field:NotNull
-    var permissions: List<PermissionRequest>,
+    val permissions: List<PermissionRequest>,
 )

0f0df7a17de17a62b184ea9bd3361ec1f70fdd71
mym1207 <mym1207@pluxity.com>
2026-09-15 13:11:17 +0900

    fix: 구독 알림 처리에서 Feature.site 프록시 초기화로 터지던 LazyInitializationException 수정
    requiredSiteId가 프록시의 requiredId를 불러 세션 밖에서 초기화를 시도했다.
    프록시가 초기화 없이 돌려주는 id를 직접 읽도록 바꾼다.

---
+++ src/test/kotlin/com/pluxity/aiot/feature/FeatureLazySiteIdKoTest.kt
@@ -0,0 +1,50 @@
+package com.pluxity.aiot.feature
+
+import com.pluxity.aiot.fixture.SiteFixture
+import com.pluxity.aiot.site.SiteRepository
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.extensions.spring.SpringTestLifecycleMode
+import io.kotest.matchers.shouldBe
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.transaction.support.TransactionTemplate
+
+/**
+ * 구독 알림 처리는 트랜잭션 밖에서 Feature를 읽는다. site는 LAZY 프록시라
+ * id 외의 접근은 세션이 없어 터진다. 실제 세션 경계로만 드러나므로 mock으로는 못 잡는다.
+ */
+@SpringBootTest
+@ActiveProfiles("test")
+class FeatureLazySiteIdKoTest(
+    private val featureRepository: FeatureRepository,
+    private val siteRepository: SiteRepository,
+    transactionManager: PlatformTransactionManager,
+) : BehaviorSpec({
+        extension(SpringExtension(SpringTestLifecycleMode.Root))
+
+        val transaction = TransactionTemplate(transactionManager)
+
+        afterEach {
+            featureRepository.deleteAll()
+            siteRepository.deleteAll()
+        }
+
+        Given("site가 배정된 Feature") {
+            val siteId =
+                transaction.execute {
+                    val site = siteRepository.save(SiteFixture.create(name = "현장"))
+                    featureRepository.save(Feature(deviceId = "SNIOT-P-WFL-001", objectId = "34957").apply { this.site = site })
+                    site.requiredId
+                }!!
+
+            When("트랜잭션 밖에서 조회해 siteId를 읽으면") {
+                val feature = featureRepository.findByDeviceId("SNIOT-P-WFL-001")!!
+
+                Then("프록시를 초기화하지 않고 id를 돌려준다") {
+                    feature.requiredSiteId shouldBe siteId
+                }
+            }
+        }
+    })
